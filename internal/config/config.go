05e3c6d27d293832559bf8664a32cba4ee57b3d0
Evgen-Poloniy <poloniy.nyrt@gmail.com>
2026-02-13 23:25:39 +0300

    Added constants and added checking of loading config and envs

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,27 +1,64 @@
 package config
 
 import (
-	"net/http"
+	"fmt"
 	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
-// Server config with tags from cleanenv library
+// Config with tags from cleanenv library
 type ServerConfig struct {
-	Host           string        `yaml:"-"`
-	Port           string        `yaml:"-"`
-	Handler        http.Handler  `yaml:"-"`
+	Host           string        `env:"API_HOST" env-required:"true"`
+	Port           string        `env:"API_PORT" env-required:"true"`
 	MaxHeaderBytes int           `yaml:"max_header_bytes" env-default:"1048576"`
 	ReadTimeout    time.Duration `yaml:"read_timeout" env-default:"4s"`
 	WriteTimeout   time.Duration `yaml:"write_timeout" env-default:"10s"`
 }
 
+// Database config from env and config.yaml
+type DatabaseConfig struct {
+	Username string `env:"DATABASE_USERNAME" env-required:"true"`
+	Password string `env:"DATABASE_PASSWORD" env-required:"true"`
+	SSLMode  string `yaml:"ssl_mode" env-required:"true"`
+}
+
+// Logger config from config.yaml
+type LoggerConfig struct {
+	Level  string `yaml:"level" env-default:"info"`
+	Format string `yaml:"format" env-default:"json"`
+}
+
+// Dataclass with all configs
+type Config struct {
+	Server   ServerConfig   `yaml:"server"`
+	Database DatabaseConfig `yaml:"database"`
+	Logger   LoggerConfig   `yaml:"logger"`
+}
+
 // Load config from config/config.yaml
-func LoadConfig(path string) (ServerConfig, error) {
-	var config ServerConfig
+func LoadConfig(path string) (Config, error) {
+	var config Config
 	if err := cleanenv.ReadConfig(path, &config); err != nil {
-		return ServerConfig{}, err
+		return Config{}, err
+	}
+
+	switch config.Logger.Level {
+	case DebugLevel, InfoLevel, WarnLevel, ErrorLevel:
+	default:
+		return Config{}, fmt.Errorf(`selected incorrect logger level: "%s"`, config.Logger.Level)
+	}
+
+	switch config.Logger.Format {
+	case TextFormat, JsonFormat:
+	default:
+		return Config{}, fmt.Errorf(`selected incorrect logger format: "%s"`, config.Logger.Format)
+	}
+
+	switch config.Database.SSLMode {
+	case SSLDisable, SSLRequire:
+	default:
+		return Config{}, fmt.Errorf(`selected incorrect ssl mode: "%s"`, config.Database.SSLMode)
 	}
 
 	return config, nil
