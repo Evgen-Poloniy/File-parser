baef47addada69f5696d94d0c9d7cd4cb90d760e
Evgen-Poloniy <poloniy.nyrt@gmail.com>
2026-02-15 22:47:44 +0300

    Fixed bugs, added graceful shutdown handlers for gorutine function like scanner and workers pull (parsers)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,16 +4,23 @@ import (
 	"context"
 	"errors"
 	"file-parser/internal/config"
+	"file-parser/internal/database"
 	"file-parser/internal/handler"
+	"file-parser/internal/logutil"
+	"file-parser/internal/parser"
 	"file-parser/internal/repository"
+	"file-parser/internal/scanner"
 	"file-parser/internal/server"
 	"file-parser/internal/service"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -27,33 +34,87 @@ func main() {
 		log.Fatalf("error when loading config: %v", err)
 	}
 
-	repository := repository.NewRepository(nil)
-	service := service.NewService(repository)
-	handler := handler.NewHandler(service)
+	logger := logrus.New()
+	logger.SetOutput(os.Stdout)
+	logutil.SetLevel(logger, &config.Logger)
+	logutil.SetFormatter(logger, &config.Logger)
 
+	db, err := database.NewPostgreSQLDatabase(&config.Database)
+	if err != nil {
+		logger.Fatalf("database initialize error: %v", err)
+	}
+	defer db.Close()
+
+	repository := repository.NewRepository(db)
+	queryService := service.NewQueryService(repository)
+	parserService := service.NewParserService(repository)
+
+	handler := handler.NewHandler(queryService)
+
+	var wg sync.WaitGroup
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	wg.Add(1)
 	server := server.NewServer(&config.Server, handler.InitHandlers(&config.Logger))
-
 	go func() {
-		log.Printf("server is running on %s:%s\n", config.Server.Host, config.Server.Port)
+		defer wg.Done()
+
+		logger.Printf("server is running on %s:%s", config.Server.Host, config.Server.Port)
 		if err := server.Start(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			log.Fatalf("server error: %v", err)
+			logger.Errorf("server error: %v", err)
+			cancel()
 		}
 	}()
 
+	jobs := make(chan string, 100)
+	defer close(jobs)
+
+	// Launch scanner at goroutine
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		scanner := scanner.NewScanner(&config.Parser, logger)
+
+		scanner.Scan(ctx, jobs)
+	}()
+
+	// Launch of workers at goroutines
+	for i := 0; i < config.Parser.CountOfWorkers; i++ {
+		wg.Add(1)
+
+		fileParser := parser.NewFileParser(parserService, &config.Parser, logger)
+
+		go func() {
+			defer wg.Done()
+
+			fileParser.ParseFile(ctx, jobs)
+		}()
+	}
+
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
-	<-quit
 
-	ctx, cancel := context.WithTimeout(
+	select {
+	case <-quit:
+		cancel()
+	case <-ctx.Done():
+	}
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(
 		context.Background(),
 		config.Server.TimeForGracefulShutdown*time.Second,
 	)
-	defer cancel()
+	defer shutdownCancel()
 
-	log.Println("shutting down server")
-	if err := server.Shutdown(ctx); err != nil {
-		log.Fatalf("server forced to shutdown: %v", err)
+	logger.Println("shutting down server")
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		logger.Fatalf("server forced to shutdown: %v", err)
 	}
 
-	log.Println("server exited properly")
+	wg.Wait()
+
+	logger.Println("server exited properly")
 }
