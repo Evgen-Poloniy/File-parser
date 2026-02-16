65a7b6fe8e97ff1914043a72b6de3316d467a839
Evgen-Poloniy <poloniy.nyrt@gmail.com>
2026-02-16 20:53:15 +0300

    Backup

--- internal/entity/file.go
+++
@@ -1,6 +0,0 @@
-package entity
-
-// Dataclass for writing information about parsed file in the database
-type File struct {
-	Filename string `db:"filename"`
-}
--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"file-parser/internal/dto"
 	"file-parser/internal/entity"
+	"strings"
 )
 
 type PostgresRepository struct {
@@ -32,8 +33,22 @@ func (p *PostgresRepository) CheckRecordAboutFile(ctx context.Context, filename 
 	return id, nil
 }
 
+func (p *PostgresRepository) CheckRecordAboutError(ctx context.Context, filename string) (int64, error) {
+	var id int64
+	query := "SELECT id FROM not_parsed_files WHERE filename = $1"
+	if err := p.db.QueryRowContext(ctx, query, filename).Scan(&id); err != nil {
+		if err == sql.ErrNoRows {
+			return 0, nil
+		}
+
+		return 0, err
+	}
+
+	return id, nil
+}
+
 // Insert information about file and parsed data into the database by one transaction
-func (p *PostgresRepository) InsertParsedData(ctx context.Context, file *entity.File, devices []entity.Device, deviceData []entity.DeviceData) error {
+func (p *PostgresRepository) InsertParsedData(ctx context.Context, filename string, devices []entity.Device, deviceData []entity.DeviceData) error {
 	tx, err := p.db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelReadCommitted})
 	if err != nil {
 		return err
@@ -49,7 +64,7 @@ func (p *PostgresRepository) InsertParsedData(ctx context.Context, file *entity.
 		VALUES ($1)
 		RETURNING id
 		`,
-		file.Filename,
+		filename,
 	).Scan(&fileID); err != nil {
 		return err
 	}
@@ -73,16 +88,157 @@ func (p *PostgresRepository) InsertParsedData(ctx context.Context, file *entity.
 	}
 
 	// Insert data about devices
+	stmt, err := tx.PrepareContext(ctx, `
+		INSERT INTO device_data (
+			n, mqtt, msg_id, text, context, class, level, area, addr, block, type, bit, invert_bit, device_id
+		) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)
+	`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
 	for _, dd := range deviceData {
-		_, err := tx.ExecContext(
+		_, err := stmt.ExecContext(
 			ctx,
-			`
-			INSERT INTO device_data (
-				n, mtqq, msg_id, text, context, class, level, area, addr, block, type, bit, invert_bit, device_id
-			) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)
-			`,
 			dd.N, dd.MQTT, dd.MsgID, dd.Text, dd.Context, dd.Class, dd.Level, dd.Area,
 			dd.Addr, dd.Block, dd.Type, dd.Bit, dd.InvertBit, deviceMap[dd.UnitGUID],
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	// for _, dd := range deviceData {
+	// 	_, err := tx.ExecContext(
+	// 		ctx,
+	// 		`
+	// 		INSERT INTO device_data (
+	// 			n, mtqq, msg_id, text, context, class, level, area, addr, block, type, bit, invert_bit, device_id
+	// 		) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)
+	// 		`,
+	// 		dd.N, dd.MQTT, dd.MsgID, dd.Text, dd.Context, dd.Class, dd.Level, dd.Area,
+	// 		dd.Addr, dd.Block, dd.Type, dd.Bit, dd.InvertBit, deviceMap[dd.UnitGUID],
+	// 	)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// }
+
+	return tx.Commit()
+}
+
+// Get data about device by unit_guid
+func (p *PostgresRepository) GetDataByUnitGUID(ctx context.Context, unitGUID string, limit int, page int) ([]dto.Response, error) {
+	offset := (page - 1) * limit
+
+	query := `
+	SELECT
+		dd.n,
+		d.unit_guid,
+		d.inv_id,
+		dd.mqtt,
+		dd.msg_id,
+		dd.text,
+		dd.context,
+		dd.class,
+		dd.level,
+		dd.area,
+		dd.addr,
+		dd.block,
+		dd.type,
+		dd.bit,
+		dd.invert_bit
+	FROM device_data dd
+	JOIN devices d ON dd.device_id = d.id
+	WHERE d.unit_guid = $1
+	ORDER BY dd.id ASC
+	LIMIT $2 OFFSET $3
+	`
+
+	rows, err := p.db.QueryContext(ctx, query, unitGUID, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var results []dto.Response
+
+	for rows.Next() {
+		var r dto.Response
+		err := rows.Scan(
+			&r.N, &r.MQTT, &r.InvID, &r.UnitGUID,
+			&r.MsgID, &r.Text, &r.Context, &r.Class,
+			&r.Level, &r.Area, &r.Addr, &r.Block,
+			&r.Type, &r.Bit, &r.InvertBit,
+		)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, r)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}
+
+// Insert information about parse errors into the database
+func (p *PostgresRepository) InsertErrors(ctx context.Context, filename string, parseErrors *entity.ParseErrors) error {
+	tx, err := p.db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelReadCommitted})
+	if err != nil {
+		return err
+	}
+
+	defer tx.Rollback()
+
+	var fileID int64
+	err = tx.QueryRowContext(
+		ctx,
+		`
+		INSERT INTO not_parsed_files(filename)
+		VALUES($1)
+		RETURNING id
+		`,
+		filename,
+	).Scan(&fileID)
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.PrepareContext(
+		ctx,
+		`
+		INSERT INTO file_errors(file_id, line, line_data, error_msg)
+		VALUES($1, $2, $3, $4)
+		`,
+	)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	for _, e := range parseErrors.Errors {
+		var lineData *string
+		if len(e.LineData) > 0 {
+			s := strings.Join(e.LineData, "\t")
+			lineData = &s
+		}
+
+		var lineNum interface{}
+		if e.LineNum != nil {
+			lineNum = *e.LineNum
+		} else {
+			lineNum = nil
+		}
+
+		_, err = stmt.ExecContext(ctx,
+			fileID,
+			lineNum,
+			lineData,
+			e.Err.Error(),
 		)
 		if err != nil {
 			return err
@@ -92,7 +248,40 @@ func (p *PostgresRepository) InsertParsedData(ctx context.Context, file *entity.
 	return tx.Commit()
 }
 
-// Get data about device by unit_guid
-func (p *PostgresRepository) GetData(unitGUID string, limit int, page int) ([]dto.Response, error) {
-	return nil, nil
+// Get information about parse errors
+func (p *PostgresRepository) GetFileErrorsByFilename(ctx context.Context, filename string, limit int, page int) ([]dto.FileErrorInfo, error) {
+	offset := (page - 1) * limit
+
+	rows, err := p.db.QueryContext(ctx, `
+        SELECT
+            f.filename,
+            e.line,
+            e.line_data,
+            e.error_msg
+        FROM file_errors e
+        JOIN not_parsed_files f ON e.file_id = f.id
+        WHERE f.filename = $1
+        ORDER BY f.id ASC
+		LIMIT $2 OFFSET $3
+    `, filename, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var errors []dto.FileErrorInfo
+	for rows.Next() {
+		var fe dto.FileErrorInfo
+		err := rows.Scan(&fe.Filename, &fe.Line, &fe.LineData, &fe.ErrorMsg)
+		if err != nil {
+			return nil, err
+		}
+		errors = append(errors, fe)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return errors, nil
 }
